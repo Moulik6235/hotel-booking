6a21ddb705eb6787955ff1977bcd4b0ed8ee4716
Moulik6235 <manochamoulik@gmail.com>
2025-07-07 13:31:35 +0530

    Controller and Routes are Created at Server Folder

--- server/server.js
+++ server/server.js
@@ -1,9 +1,10 @@
-import express from  "express"
+import express from "express"
 import "dotenv/config";
 import cors from "cors";
 import connectDB from "./configs/db.js";
 import { clerkMiddleware } from '@clerk/express'
 import clerkWebhooks from "./controllers/clerkWebhooks.js";
+import userRouter from "./routes/userRoutes.js";
 
 connectDB()
 
@@ -17,8 +18,9 @@ app.use(clerkMiddleware())
 // API to listen to Clerk Webhooks
 app.use("/api/clerk", clerkWebhooks);
 
-app.get('/', (req,res)=> res.send("API is Working"))
+app.get('/', (req, res) => res.send("API is Working"))
+app.use('/api/user', userRouter)
 
 const PORT = process.env.PORT || 3000;
 
-app.listen(PORT, ()=> console.log(`Server running on port ${PORT}`));
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
